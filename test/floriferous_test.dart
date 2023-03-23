95fde05218b5e12c4fbb6f5be558062f4cea555e
Jesus Santos <jesus.santos364@gmail.com>
2023-03-23 16:47:13 -0700

    Creada clase Card

--- test/floriferous_test.dart
+++ test/floriferous_test.dart
@@ -6,3 +6,22 @@ void main() {
     expect(calculate(), 42);
   });
 }
+
+enum Flowers {Daisy, Lily, Mum, Poppy, Tulip}
+enum Colors {White, Yellow, Orange, Pink, Purple}
+enum Bugs {Bee, Beetle, Butterfly, Ladybug, Moth}
+
+class Card{
+  late final Flowers _flower;
+  late final Colors _color;
+  late final Bugs _bug; 
+
+  Flowers get flower => _flower;
+  Colors get color => _color;
+  Bugs get bug => _bug;
+
+}
+
+class ArrangementCard extends Card{
+  
+}
